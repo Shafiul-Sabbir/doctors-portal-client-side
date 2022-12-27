bdddf899ac4d9f8ad7d048b6b060fbaaf852f0e1
Shafiul-Sabbir <sabbirvai82@gmail.com>
2022-12-28 00:07:21 +0600

    changes

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  themes: [
+    {
+      doctortheme: {
+        primary: "#3ABFF8",
+        secondary: "#f6d860",
+        accent: "#37cdbe",
+        neutral: "#3d4451",
+        "base-100": "#ffffff",
+      },
+    },
+    "dark",
+    "cupcake",
+  ],
+  plugins: [require("daisyui")],
+}
