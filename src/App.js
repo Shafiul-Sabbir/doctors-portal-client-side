4d123a6db3068b1219e59eec585635cd9dcd3820
Shafiul-Sabbir <sabbirvai82@gmail.com>
2022-12-28 02:13:51 +0600

    changes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Navbar from './Pages/Shared/Navbar';
 import Home from './Pages/Home/Home';
 import About from './Pages/About/About';
+import Login from './Pages/Login/Login';
 function App() {
   return (
     <div >
@@ -10,6 +11,8 @@ function App() {
      <Routes>
       <Route path="/" element={<Home/>}/>
       <Route path="about" element={<About/>}/>
+      <Route path="login" element={<Login/>}/>
+
       </Routes>   
     </div>
   );
